d7562c2dcd867266b699380fb98b757e8c8b8ca2
“anildalar” <dollorinfotech@gmail.com>
2022-05-18 09:30:48 +0530

    Variables

---
+++ index.js
@@ -0,0 +1,18 @@
+// Comment
+
+// Variable Decleration
+
+var x; // Statement 1
+
+// Variable Initialization
+x=10;
+
+
+var y=20;  // Statement 2
+
+
+ var result = x + y;
+
+
+console.log(result)
+
